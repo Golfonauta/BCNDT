2531b531d6afe609e52aa30234d135162d509ed9
Golfonauta <golfonauta@gmail.com>
2012-06-30 17:35:07 +0200

    first commit

---
+++ Lector RSS Incidencias/bin/Debug/AppX/js/default.js
@@ -0,0 +1,38 @@
+﻿// Para obtener una introducción a la plantilla En blanco, consulte la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    WinJS.strictProcessing();
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: Esta aplicación se ha iniciado recientemente. Inicializar
+                // la aplicación aquí.
+            } else {
+                // TODO: Esta aplicación se ha reactivado tras estar suspendida.
+                // Restaurar el estado de la aplicación aquí.
+            }
+            //incidentList = new WinJS.Binding.List();
+            //var publicMembers = { ItemList: articlesList };
+            //WinJS.Namespace.define("C9Data", publicMembers);
+
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: Esta aplicación está a punto de suspenderse. Guardar cualquier estado
+        // que deba mantenerse a través de las suspensiones aquí. Puede usar el
+        // objeto WinJS.Application.sessionState, que se guarda y se restaura
+        // automáticamente en las suspensiones. Si debe completar una
+        // operación asincrónica antes de suspenderse la aplicación, llame a
+        // args.setPromise().
+    };
+
+    app.start();
+})();
