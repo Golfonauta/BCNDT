7216d2ce88197ee5621c8c268f9a4cd58a27c9e9
Pol Gómez Guerrero <thebestpol@gmail.com>
2012-07-01 12:40:56 +0200

    Template update

---
+++ App1/pages/items/items.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    "use strict";
+
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    var ui = WinJS.UI;
+
+    ui.Pages.define("/pages/items/items.html", {
+
+        // This function updates the ListView with new layouts
+        initializeLayout: function (listView, viewState) {
+            /// <param name="listView" value="WinJS.UI.ListView.prototype" />
+
+            if (viewState === appViewState.snapped) {
+                listView.layout = new ui.ListLayout();
+            } else {
+                listView.layout = new ui.GridLayout();
+            }
+        },
+
+        itemInvoked: function (args) {
+            var groupKey = Data.groups.getAt(args.detail.itemIndex).key;
+            WinJS.Navigation.navigate("/pages/split/split.html", { groupKey: groupKey });
+        },
+
+        // Se llama a esta función cuando un usuario navega a esta página. Esta
+        // rellena los elementos de la página con los datos de la aplicación.
+        ready: function (element, options) {
+            var listView = element.querySelector(".itemslist").winControl;
+            listView.itemDataSource = Data.groups.dataSource;
+            listView.itemTemplate = element.querySelector(".itemtemplate");
+            listView.oniteminvoked = this.itemInvoked.bind(this);
+
+            this.initializeLayout(listView, Windows.UI.ViewManagement.ApplicationView.value);
+            listView.element.focus();
+        },
+
+        // Esta función actualiza el diseño de la página como respuesta a los cambios en viewState.
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+            /// <param name="viewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+            /// <param name="lastViewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+
+            var listView = element.querySelector(".itemslist").winControl;
+            if (lastViewState !== viewState) {
+                if (lastViewState === appViewState.snapped || viewState === appViewState.snapped) {
+                    var handler = function (e) {
+                        listView.removeEventListener("contentanimating", handler, false);
+                        e.preventDefault();
+                    }
+                    listView.addEventListener("contentanimating", handler, false);
+                    var firstVisible = listView.indexOfFirstVisible;
+                    this.initializeLayout(listView, viewState);
+                    listView.indexOfFirstVisible = firstVisible;
+                }
+            }
+        }
+    });
+})();
---
+++ Lector RSS Incidencias/bin/Debug/AppX/js/data.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    var dataArray = [
+    { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "images/60banana.png" },
+    { title: "Banana blast", text: "Ice cream", picture: "images/60banana.png" },
+    { title: "Brilliant banana", text: "Frozen custard", picture: "images/60banana.png" },
+    { title: "Orange surprise", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Original orange", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Vanilla", text: "Ice cream", picture: "images/60vanilla.png" },
+    { title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
+    { title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" },
+     { title: "Brilliant banana", text: "Frozen custard", picture: "images/60banana.png" },
+    { title: "Orange surprise", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Original orange", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Vanilla", text: "Ice cream", picture: "images/60vanilla.png" },
+    { title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
+    { title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" }, { title: "Brilliant banana", text: "Frozen custard", picture: "images/60banana.png" },
+    { title: "Orange surprise", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Original orange", text: "Sherbet", picture: "images/60orange.png" },
+    { title: "Vanilla", text: "Ice cream", picture: "images/60vanilla.png" },
+    { title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
+    { title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" },
+    { title: "Succulent strawberry", text: "Sorbet", picture: "images/60strawberry.png" }
+    ];
+
+    var dataList = new WinJS.Binding.List(dataArray);
+
+    // Create a namespace to make the data publicly
+    // accessible. 
+    var publicMembers =
+        {
+            itemList: dataList
+        };
+    WinJS.Namespace.define("DataExample", publicMembers);
+
+})();
